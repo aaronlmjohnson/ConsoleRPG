7b26caaf504550a9d797dbffb5167ca8a99bf0f3
ajonaaronajon <ajonaaronajon@yahoo.com>
2021-02-05 19:04:33 -0600

    initial commit

---
+++ ConsoleRPG/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleRPG
+{
+    class Program
+    {
+        class Window
+        {
+            private int[,] window;
+            public Window(int row, int col)
+            {
+                window = new int[row, col];
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
