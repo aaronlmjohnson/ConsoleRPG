a826ea680e8601f68f6396640d77872e5d4c2ce9
ajonaaronajon <ajonaaronajon@yahoo.com>
2021-02-05 23:59:37 -0600

    created Window Class

--- ConsoleRPG/Program.cs
+++ ConsoleRPG/Program.cs
@@ -6,15 +6,58 @@ namespace ConsoleRPG
     {
         class Window
         {
-            private int[,] window;
-            public Window(int row, int col)
+            private char[,] window;
+            private int width = Console.WindowWidth;
+            private int height = Console.WindowHeight;
+            public Window()
             {
-                window = new int[row, col];
+                window = new char[height, width];
+            }
+
+            public void Fill(char fillChar)
+            {
+                for(int i = 0; i < height; i++)
+                {
+                    for(int j = 0; j < width; j++)
+                    {
+                        window[i, j] = fillChar;
+                    }
+                }
+            }
+            
+            public void Display()
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    string row = "";
+                    for (int j = 0; j < width; j++)
+                    {
+                        row += window[i, j];
+                    }
+                    Console.WriteLine(row);
+                }
+            }
+
+            public void Draw(int rowStart, int colStart, char fill)
+            {
+                int objWidth = 5;
+                int objHeight = 3;
+
+                for (int i = rowStart; i < (rowStart + objHeight); i++)
+                {
+                    for (int j = colStart; j < (colStart + objWidth); j++)
+                    {
+                        window[i, j] = fill;
+                    }
+                }
             }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Window screen = new Window();
+            screen.Fill('.');
+            screen.Draw(0, 0, '#');
+            screen.Display();
         }
     }
 }
