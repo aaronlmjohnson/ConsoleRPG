c94588eacaf06496b484dd73b702c1d1a106d584
ajonaaronajon <ajonaaronajon@yahoo.com>
2021-02-06 00:27:40 -0600

    created GameObject class

--- ConsoleRPG/Program.cs
+++ ConsoleRPG/Program.cs
@@ -52,12 +52,61 @@ namespace ConsoleRPG
                 }
             }
         }
+
+        class GameObject
+        {
+            private int width;
+            private int height;
+            private int x;
+            private int y;
+            private char[,] body;
+            public GameObject(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+                LoadAsset(@"C:\Users\Aaron\Desktop\c# projects\ConsoleRPG\assets\shop.txt");  
+            }
+
+            private void LoadAsset(string filePath)
+            {
+                string[] assetInfo;
+                assetInfo = System.IO.File.ReadAllLines(filePath);
+                width = assetInfo[0].Length;
+                height = assetInfo.Length;
+
+                body = new char[height, width];
+
+                for(int i = 0; i < height; i++)
+                {
+                    for(int j = 0; j < width; j++)
+                    {
+                        body[i, j] = assetInfo[i][j];
+                    }
+                }
+            }
+
+            public void Display()
+            {
+                for(int i = 0; i < height; i++)
+                {
+                    string row = "";
+                    for(int j = 0; j < width; j++)
+                    {
+                        row += body[i, j];
+                    }
+                    Console.WriteLine(row);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Window screen = new Window();
-            screen.Fill('.');
-            screen.Draw(0, 0, '#');
-            screen.Display();
+            //Window screen = new Window();
+            //screen.Fill('.');
+            //screen.Draw(0, 0, '#');
+            //screen.Display();
+
+            GameObject shop = new GameObject(0, 0);
+            shop.Display();
         }
     }
 }
